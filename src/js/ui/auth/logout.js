907db4b4040ca3678293ad12148bb26edca7745a
rikkejuliane <rikand01864@stud.noroff.no>
2024-11-20 12:09:10 +0100

    Jsdocs the logout and cleaned up the code

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -1,9 +1,11 @@
 /**
- * This function should log the user out by removing aproppriate user data from the browser.
+ * Logs out the user by clearing relevant user data from the browser's localStorage.
+ *
+ * This function removes the user's token and username from localStorage, effectively 
+ * logging them out. Optionally, it can redirect the user to the login page if desired.
  */
 
 export function onLogout() {
   localStorage.removeItem("token");
   localStorage.removeItem("username");
-  //window.location.href = "/auth/login/";
 }
--- src/js/ui/global/logout.js
+++ src/js/ui/global/logout.js
@@ -1,8 +1,16 @@
 import { onLogout } from "../auth/logout.js";
 
 /**
- * Functions you attach to logout events that calls ui/auth/logout function
+ * Attaches a logout event listener to the logout button.
+ *
+ * This function selects the logout button from the DOM and attaches a click 
+ * event listener to it. When the button is clicked, the `onLogout` function is 
+ * called to clear user data, and the user is redirected to the login page.
+ *
+ * @async
+ * @throws {Error} If the logout button is not found in the DOM.
  */
+
 export async function setLogoutListener() {
   const logoutButton = document.querySelector("#logout-btn");
   logoutButton.addEventListener("click", () => {

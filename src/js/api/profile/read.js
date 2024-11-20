f1fbb3332ecd9a880e6484adc4a2f06107273038
rikkejuliane <rikand01864@stud.noroff.no>
2024-11-20 18:31:53 +0100

    Fetched the posts o home and single blogpost

--- src/js/api/profile/read.js
+++ src/js/api/profile/read.js
@@ -1,3 +1,10 @@
-export async function readProfile(username) {}
+import { authGuard } from "../../utilities/authGuard.js";
+import { setLogoutListener } from "../../ui/global/logout.js";
+import { renderPosts, readPostsByUser } from "../../api/post/read.js";
 
-export async function readProfiles(limit, page) {}
+authGuard();
+setLogoutListener();
+
+const userPostsContainer = document.querySelector("#user-posts-container");
+const username = "exampleUsername"; // Replace with dynamic username
+renderPosts(userPostsContainer, readPostsByUser, { username });
--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,6 +1,9 @@
-import { authGuard } from "../../utilities/authGuard";
-import { setLogoutListener } from '../../ui/global/logout.js';
-
+import { authGuard } from "../../utilities/authGuard.js";
+import { setLogoutListener } from "../../ui/global/logout.js";
+import { renderPosts, readPosts } from "../../api/post/read.js";
 
 authGuard();
 setLogoutListener();
+
+const feedContainer = document.querySelector("#feed-container");
+renderPosts(feedContainer, readPosts);

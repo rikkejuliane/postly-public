e11e0ca21deec4fdf6053940b90f316e06cbd98f
rikkejuliane <rikand01864@stud.noroff.no>
2024-11-22 11:17:16 +0100

    cleaned up some code

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -4,24 +4,19 @@ import { authGuard } from "../../utilities/authGuard.js";
 import { setLogoutListener } from "../../ui/global/logout.js";
 import { renderPosts, readPost } from "../../api/post/read.js";
 
-// Ensure the user is authenticated
 authGuard();
-
-// Set up the logout button functionality
 setLogoutListener();
 
-// Select the container for rendering the single post
+
 const postContainer = document.querySelector("#post-container");
 
-// Retrieve the post ID from the URL query parameters
 const urlParams = new URLSearchParams(window.location.search);
 const postId = urlParams.get("id");
 
 if (postId) {
   renderPosts(postContainer, async () => {
-    const post = await readPost(postId); // Fetch the post
-    console.log("Fetched Single Post:", post); // Debug log
-    return { data: [post] }; // Wrap the single post in an array for compatibility with renderPosts
+    const post = await readPost(postId);
+    return { data: [post] };
   });
 } else {
   postContainer.innerHTML = "<p>Post ID is missing in the URL.</p>";
